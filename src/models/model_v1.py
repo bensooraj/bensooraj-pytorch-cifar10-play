7489199c9e303cce7f0d461445fb69ea8fa44840
bensooraj <bensooraj@users.noreply.github.com>
2025-10-03 23:18:44 +0530

    The model is now responding!

--- src/models/model_v1.py
+++ src/models/model_v1.py
@@ -11,53 +11,79 @@ class ModelV1(nn.Module):
         # Block 01 | Entry
         self.block01 = nn.Sequential(
             # Layer 01
-            nn.Conv2d(3, 32, 1, padding=0),
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=16,
+                kernel_size=7,
+                stride=1,
+                padding=3,
+                bias=True,
+            ),
             nn.ReLU(),
             # Layer 02
-            nn.Conv2d(32, 32, 3, padding=0),
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+                bias=True,
+            ),
             nn.ReLU(),
             # Layer 03
-            nn.Conv2d(32, 64, 1, padding=0),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                bias=True,
+            ),
             nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(self.DROPOUT_VALUE),
         )
         # Block 01 | Depthwise Separable
         self.block02 = nn.Sequential(
             # Layer 01
-            nn.Conv2d(64, 256, 1, padding=0),
+            nn.Conv2d(64, 128, 3, padding=0),
             nn.ReLU(),
             # Layer 02 | Depthwise
-            nn.Conv2d(256, 256, 3, padding=1, groups=256),
+            nn.Conv2d(128, 128, 3, padding=1, groups=128),
             nn.ReLU(),
             # Layer 03 | Pointwise
-            nn.Conv2d(256, 128, 1, padding=0),
+            nn.Conv2d(128, 128, 1, padding=0),
             nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Dropout(self.DROPOUT_VALUE),
         )
         # Block 03
         self.block03 = nn.Sequential(
             # Layer 01
-            nn.Conv2d(128, 64, 1, padding=0),
+            nn.Conv2d(128, 32, 3, padding=1),
             nn.ReLU(),
             # Layer 02 | Dilated Convolution
-            nn.Conv2d(64, 64, 3, padding=0, dilation=2),
+            nn.Conv2d(32, 32, 3, padding=0, dilation=2),
             nn.ReLU(),
             # Layer 03
-            nn.Conv2d(64, 128, 1, padding=0),
+            nn.Conv2d(32, 64, 1, padding=0),
             nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(self.DROPOUT_VALUE),
         )
         # Block 04
         self.block04 = nn.Sequential(
             # Layer 01
-            nn.Conv2d(128, 64, 1, padding=0),
+            nn.Conv2d(64, 32, 3, padding=1),
             nn.ReLU(),
             # Layer 02
-            nn.Conv2d(64, 64, 3, padding=0),
+            nn.Conv2d(32, 10, 3, padding=0),
             nn.ReLU(),
-            # Layer 03
-            nn.Conv2d(64, 128, 1, padding=0),
-            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(self.DROPOUT_VALUE),
         )
         # Block 05
-        self.block05 = nn.Sequential(nn.AvgPool2d(1))
+        self.block05 = nn.Sequential(nn.AdaptiveAvgPool2d((1, 1)))
 
     def forward(self, x):
         x = self.block01(x)
